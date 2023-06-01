01c0a591039274ac5bcda709d9efe18224d15002
Daria Dotsenko <dotsenko8268@gmail.com>
2023-06-01 17:11:23 +0300

    3.4.4. Обработка POST-запроса

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http')
 const chalk = require('chalk')
 const fs = require('fs/promises')
 const path = require('path')
+const { addNote } = require('./notes.controller')
 
 const port = 3000
 
@@ -15,6 +16,22 @@ const server = http.createServer(async (req, res) => {
             'Content-Type': 'text/html'
         })
         res.end(content)
+    } else if (req.method === 'POST') {
+        const body = []
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Disposition': 'inline' // or 'attachment' if you want to force download
+        });
+
+        req.on('data', data => {
+            body.push(Buffer.from(data))
+        })
+
+        req.on('end', () => {
+            const title = body.toString().split('=')[1].replace(/\+/g, ' ')
+            addNote(title)
+            res.end(`Title = ${title}`)
+        })
     }
 })
 
