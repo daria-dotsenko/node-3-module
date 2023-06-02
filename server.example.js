455f8133bd48698e1be4af09fb46bba56204bd4a
Daria Dotsenko <dotsenko8268@gmail.com>
2023-06-02 10:25:47 +0300

    3.4.5. Работа с Express.js

---
+++ server.example.js
@@ -0,0 +1,26 @@
+const server = http.createServer(async (req, res) => {
+    if (req.method === 'GET') {
+        const content = await fs.readFile(path.join(basePath, 'index.html'))
+        // res.setHeader('Content-Type', 'text/html')
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.end(content)
+    } else if (req.method === 'POST') {
+        const body = []
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Disposition': 'inline' // or 'attachment' if you want to force download
+        });
+
+        req.on('data', data => {
+            body.push(Buffer.from(data))
+        })
+
+        req.on('end', () => {
+            const title = body.toString().split('=')[1].replace(/\+/g, ' ')
+            addNote(title)
+            res.end(`Title = ${title}`)
+        })
+    }
+})
